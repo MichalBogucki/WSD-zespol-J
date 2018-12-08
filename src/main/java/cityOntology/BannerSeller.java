78371e89069fceda97f5bd1e7241d2756ae76cd7
bogucek4 <bogucek4@gazeta.pl>
2018-12-08 15:32:03 +0100

    kolejna ontologia

--- src/main/java/cityOntology/BannerSeller.java
+++
@@ -1,4 +0,0 @@
-package cityOntology;
-
-public class BannerSeller {
-}
--- src/main/java/cityOntology/CityOntology.java
+++ src/main/java/cityOntology/CityOntology.java
@@ -1,6 +1,5 @@
 package cityOntology;
 
-import agents.HelloAgent;
 import jade.content.onto.*;
 import jade.content.schema.ConceptSchema;
 
@@ -10,21 +9,27 @@ public class CityOntology extends Ontology {
     public static final String ONTOLOGY_NAME = "CityOntology";
 
     // VOCABULARY
-    public static final String OFFER = "Offer";
-    public static final String AUCTION = "Auction";
-
     public static final String BANNER = "Banner";
-    public static final String BANNER_SELLER = "Seller";
-    public static final String BANNER_BUYER = "Buyer";
-    public static final String BANNER_SOLD = "BannerSold";
-    public static final String BANNER_WINNER = "BannerWinner";
-
-    public static final String BANNER_SUBSCRIPTION_COST = "BannerSubscriptionCost";
-    public static final String BUYER_SUBSCRIPTION_OFFER = "BuyerSubscriptionOffer";
-
-    public static final String TRAFFIC_NODE = "TrafficNode";
+    public static final String NODE_NUMBER = "NodeNumber";
     public static final String POPULATION = "Population";
+    public static final String TIME_FRAME = "TimeFrame";
+    public static final String IS_SOLD = "BannerSold";
 
+    public static final String ADVERTISEMENT_SELLER = "Seller";
+    public static final String SELLER_NAME = "SellerName";
+    public static final String SELLER_BANNER = "SellerBanner";
+    public static final String SELLER_SUBSCRIPTION_COST = "SubscriptionCost";
+    public static final String SELLER_SUBSCRIBENTS = "Subscribents";
+    public static final String SELLER_AUCTION_WINNER = "AuctionWinner";
+
+    public static final String ADVERTISEMENT_BUYER = "Buyer";
+    public static final String BUYER_SUBSCRIPTION_OFFER = "SubscriptionOffer";
+    public static final String BUYER_SUBSCRIBED_BANNERS = "SubscribedBanners";
+
+    public static final String DATA_GENERATOR = "DataGenerator";
+    public static final String DATA_GENERATOR_NAME = "Name";
+    public static final String DATA_GENERATOR_POPULATION = "Population";
+    public static final String DATA_GENERATOR_LOCAL_BANNERS = "LocalBanners";
 
     // The singleton instance of this ontology
     private static Ontology theInstance = new CityOntology();
@@ -38,18 +43,24 @@ public class CityOntology extends Ontology {
         super(ONTOLOGY_NAME, BasicOntology.getInstance());
 
         try {
-            add(new ConceptSchema(OFFER), Offer.class);
-            add(new ConceptSchema(AUCTION), Auction.class);
-            add(new ConceptSchema(BANNER), Banner.class);
-            add(new ConceptSchema(BANNER_BUYER), BannerBuyer.class);
-            add(new ConceptSchema(BANNER_SELLER), BannerSeller.class);
-            add(new ConceptSchema(BANNER_SOLD), BannerSold.class);
-            add(new ConceptSchema(BANNER_WINNER), BannerWinner.class);
-            add(new ConceptSchema(BANNER_SUBSCRIPTION_COST), BannerSubscriptionCost.class);
-            add(new ConceptSchema(BUYER_SUBSCRIPTION_OFFER), BuyerSubscriptionOffer.class);
-            add(new ConceptSchema(TRAFFIC_NODE), TrafficNode.class);
-            add(new ConceptSchema(POPULATION), Population.class);
-
+            add(new ConceptSchema(BANNER ), Banner.class);
+            add(new ConceptSchema(NODE_NUMBER ), NodeNumber.class);
+            add(new ConceptSchema(POPULATION ), Population.class);
+            add(new ConceptSchema(TIME_FRAME ), TimeFrame.class);
+            add(new ConceptSchema(IS_SOLD ), IsSold.class);
+            add(new ConceptSchema(ADVERTISEMENT_SELLER), SellerAdvertisement.class);
+            add(new ConceptSchema(SELLER_NAME ), SellerName.class);
+            add(new ConceptSchema(SELLER_BANNER ), SellerBanner.class);
+            add(new ConceptSchema(SELLER_SUBSCRIPTION_COST), SubscriptionCost.class);
+            add(new ConceptSchema(SELLER_SUBSCRIBENTS ), Subscribents.class);
+            add(new ConceptSchema(SELLER_AUCTION_WINNER ), AuctionWinner.class);
+            add(new ConceptSchema(ADVERTISEMENT_BUYER), BuyerAdvertisement.class);
+            add(new ConceptSchema(BUYER_SUBSCRIPTION_OFFER ), SubscriptionOffer.class);
+            add(new ConceptSchema(BUYER_SUBSCRIBED_BANNERS ), SubscribedBanners.class);
+            add(new ConceptSchema(DATA_GENERATOR ), DataGenerator.class);
+            add(new ConceptSchema(DATA_GENERATOR_NAME ), GeneratorName.class);
+            add(new ConceptSchema(DATA_GENERATOR_POPULATION ), Population.class);
+            add(new ConceptSchema(DATA_GENERATOR_LOCAL_BANNERS ), LocalBanners.class);
 
         }
         catch (OntologyException oe){
